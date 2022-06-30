a329d4316a948fb17cb6ebe480df57004c549d89
bodascaluca <bodasca.luca@gmail.com>
2022-06-30 16:57:53 +0200

    start to implamete ComicController.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Primo argomento - come nome della tabella (in plurale)
+// Nome del controller come Model (in singolare)
+Route::resource('Comics', 'ComicController');
