c4520284d296189e1ffe8bab10ef75f5cd7d1321
bodascaluca <bodasca.luca@gmail.com>
2022-07-04 23:25:29 +0200

    Ho un problrma con l'aggiunta nel databse di quello che canceliamo

--- app/Comics.php
+++ app/Comics.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model\SoftDeletes;
+
 
 class Comics extends Model
 {
+    use SoftDeletes;
+
+    protected $table = 'comics';
+
     protected $fillable = [
         'title',
         'description',
